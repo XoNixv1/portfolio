11feaae8414285cc33ed59202b4bcfb8bcec72e2
XoNixv1 <haled14253@gmail.com>
2025-02-23 14:16:20 +0100

    small fixes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
-import "./app.scss";
 import ContactForm from "../components/contact-form/ContactForm";
 import { FormProvider } from "../hooks/FormProvider";
 import AnimatedPages from "./AnimatedPages";
+import "./app.scss";
 
 function App() {
   return (
