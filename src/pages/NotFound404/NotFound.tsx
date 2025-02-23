646be857071bce8241db44435baa84b8682485f1
XoNixv1 <haled14253@gmail.com>
2025-02-23 13:22:17 +0100

    small bug fixes

--- src/pages/NotFound404/NotFound.tsx
+++ src/pages/NotFound404/NotFound.tsx
@@ -1,3 +1,13 @@
+import "./NotFound.scss";
+
 export default function NotFound() {
-  return <div>page not existing...</div>;
+  return (
+    <div className="not-faund">
+      <p className="not-faund__404">404</p>
+      <p className="not-faund__label">Not Found</p>
+      <p className="not-faund__descr">
+        The Page you are looking for doesn`t exist or other error accured.
+      </p>
+    </div>
+  );
 }
